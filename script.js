172bfda88195ba047d7701970e13624175858920
Gabriel <gopxfs@hotmail.com>
2022-05-09 17:52:20 -0300

    Update the menu buttons functionality

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-const headerButton = document.getElementById('header-button');
-
+// A function that opens and closes the mobile menu
 function toggleMenu() {
   const menu = document.getElementById('header-flex');
   menu.classList.toggle('active');
 }
 
+// Creating the variables to apply the toggleMenu function to
+const headerButton = document.getElementById('header-button');
+const menuOptions = document.getElementsByClassName('menu-option');
+
+// When clicked, the elements are going to execute the toggleMenu function
 headerButton.addEventListener('click', toggleMenu);
+for (let i=0; i<menuOptions.length; i++) {
+  menuOptions[i].addEventListener('click', toggleMenu);
+}
