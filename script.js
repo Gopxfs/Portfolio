2ac37370c60df34d6392e644a481342042ad57d3
Gabriel <gopxfs@hotmail.com>
2022-05-10 22:36:15 -0300

    Update popup window

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ const projects = [
   {
     name: "Multi-Post Stories",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    mobileDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
     image: "images/project-images/image-placeholder3.jpg",
     technologies: ["html", "Ruby on rails", "css", "Github"],
     liveVersion: "live.link",
@@ -26,6 +27,7 @@ const projects = [
   {
     name: "Multi-Post Stories2",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    mobileDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
     image: "images/project-images/image-placeholder4.jpg",
     technologies: ["html", "Ruby on rails", "css", "Github"],
     liveVersion: "live.link",
@@ -35,6 +37,7 @@ const projects = [
   {
     name: "Multi-Post Stories3",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    mobileDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
     image: "images/project-images/image-placeholder3.jpg",
     technologies: ["html", "Ruby on rails", "css", "Github"],
     liveVersion: "live.link",
@@ -44,6 +47,7 @@ const projects = [
   {
     name: "Multi-Post Stories4",
     description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.",
+    mobileDescription: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.",
     image: "images/project-images/image-placeholder4.jpg",
     technologies: ["html", "Ruby on rails", "css", "Github"],
     liveVersion: "live.link",
@@ -52,8 +56,14 @@ const projects = [
 ];
 // Function to change the popup project properties based on which button was clicked
 function selectProject(projectNumber) {
+  const mediaQuery = window.matchMedia('(min-width: 768px)');
+  if (!mediaQuery.matches) {
+    document.getElementById('popup-description').innerHTML = projects[projectNumber].mobileDescription;
+  }
+  else {
+    document.getElementById('popup-description').innerHTML = projects[projectNumber].description;
+  }
   document.getElementById('popup-title').innerHTML = projects[projectNumber].name;
-  document.getElementById('popup-description').innerHTML = projects[projectNumber].description;
   document.getElementById('popup-image').src = projects[projectNumber].image;
   document.getElementById('live-link').href = projects[projectNumber].liveVersion;
   document.getElementById('source-link').href = projects[projectNumber].source;
