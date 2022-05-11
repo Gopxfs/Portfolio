a440c94b0df0d9f6a424a820f1945c5a51f8cc57
Gabriel <gopxfs@hotmail.com>
2022-05-11 02:04:33 -0300

    Update linter errors

--- script.js
+++ script.js
@@ -49,22 +49,21 @@ const projects = [
     technologies: ['html', 'Ruby on rails', 'css', 'Github'],
     liveVersion: 'live.link',
     source: 'source.link',
-  }
+  },
 ];
 // Function to change the popup project properties based on which button was clicked
 function selectProject(projectNumber) {
   const mediaQuery = window.matchMedia('(min-width: 768px)');
   if (!mediaQuery.matches) {
     document.getElementById('popup-description').innerHTML = projects[projectNumber].mobileDescription;
-}
-  else {
+  } else {
     document.getElementById('popup-description').innerHTML = projects[projectNumber].description;
-}
+  }
   document.getElementById('popup-title').innerHTML = projects[projectNumber].name;
   document.getElementById('popup-image').src = projects[projectNumber].image;
   document.getElementById('live-link').href = projects[projectNumber].liveVersion;
   document.getElementById('source-link').href = projects[projectNumber].source;
-  let technologiesList = document.getElementById('popup-technologies');
+  const technologiesList = document.getElementById('popup-technologies');
   let listItem;
   technologiesList.innerHTML = '';
   for (let i = 0; i < projects[projectNumber].technologies.length; i += 1) {
@@ -74,26 +73,26 @@ function selectProject(projectNumber) {
   }
 }
 // Execute the selectProject function in these variables
-  const project1 = document.getElementById('project-button1');
-  const project2 = document.getElementById('project-button2');
-  const project3 = document.getElementById('project-button3');
-  const project4 = document.getElementById('project-button4');
-  project1.addEventListener('click', function() {selectProject(0)});
-  project2.addEventListener('click', function() {selectProject(1)});
-  project3.addEventListener('click', function() {selectProject(2)});
-  project4.addEventListener('click', function() {selectProject(3)});
+const project1 = document.getElementById('project-button1');
+const project2 = document.getElementById('project-button2');
+const project3 = document.getElementById('project-button3');
+const project4 = document.getElementById('project-button4');
+project1.addEventListener('click', () => { selectProject(0); });
+project2.addEventListener('click', () => { selectProject(1); });
+project3.addEventListener('click', () => { selectProject(2); });
+project4.addEventListener('click', () => { selectProject(3); });
 
 // Function to open the pop up window
-  function togglePopupWindow() {
+function togglePopupWindow() {
   const popupWindow = document.getElementById('popup-project');
   const body = document.getElementsByTagName('BODY')[0];
   const pageFlex = document.getElementsByClassName('page-flex');
   popupWindow.classList.toggle('active');
-  body.classList.toggle ('active');
-  pageFlex[0].classList.toggle ('active');
+  body.classList.toggle('active');
+  pageFlex[0].classList.toggle('active');
 }
 // Execute the togglePopupWindow function in these variables
-  const toggleProjectWindow = document.getElementsByClassName('popup-project-window');
-  for (let i = 0; i < toggleProjectWindow.length; i += 1) {
-    toggleProjectWindow[i].addEventListener('click', togglePopupWindow);
+const toggleProjectWindow = document.getElementsByClassName('popup-project-window');
+for (let i = 0; i < toggleProjectWindow.length; i += 1) {
+  toggleProjectWindow[i].addEventListener('click', togglePopupWindow);
 }
