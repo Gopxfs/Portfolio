3ea69c64ac62bdc6104eb2d9ac7bba675ea8a30d
Gabriel <gopxfs@hotmail.com>
2022-06-09 22:12:40 -0300

    Add pokemon hunt project

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ for (let i = 0; i < menuOptions.length; i += 1) {
 // Projects properties (inside an array)
 const projects = [
   {
-    name: 'Multi-Post Stories',
+    name: 'Pokemon hunt website (fake event)',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scramble.',
     shortDescription: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting  ever since the 1500s, when an unknown printer took a galley of type veris lapoa todoe.',
     shortestDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
-    image: 'images/project-images/image-placeholder3.jpg',
+    image: 'images/project-images/pokemon-page-desktop.png',
     technologies: ['html', 'Ruby on rails', 'css', 'Github'],
-    liveVersion: 'live.link',
-    source: 'source.link',
+    liveVersion: 'https://gopxfs.github.io/Pokemon-hunt/',
+    source: 'https://github.com/Gopxfs/Pokemon-hunt',
   },
   {
     name: 'Multi-Post Stories2',
